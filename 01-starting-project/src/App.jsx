d8a0158b0a5d347e27baa2df0bc01b7c5ef009e3
Darshan MT <darshanmalli93@gmail.com>
2025-07-04 04:05:28 +0000

    dynamic listing useing  different operaters and used useState hook to rerun react component

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,12 +1,36 @@
+import { useState } from 'react'
 import componentsImg from './assets/components.png'
 import {CORE_CONCEPTS} from './data.js'
 import Header from './components/Header/Header.jsx'
 import CoreConcept from './components/CoreConcept.jsx'
 import TabButton  from './components/TabButton.jsx'
+import { EXAMPLES } from './data.js'
+
 
 
 
 function App() {
+  const [selectedTopic,setSelectedTopic] = useState();
+  function handleSelect(selectedButton){
+      
+        setSelectedTopic(selectedButton)
+  }
+  let tabContent = <p>Please Select a topic.</p>
+
+        if(selectedTopic){
+          tabContent = (
+            <div id='tab-content'>
+          <h3>{EXAMPLES[selectedTopic].title}</h3>
+          <p>{EXAMPLES[selectedTopic].description}</p>
+          <pre>
+            <code>
+                {EXAMPLES[selectedTopic].code}
+            </code>
+          </pre>
+         </div>
+          )
+        }
+   
   return (
     <div>
        <Header />
@@ -14,25 +38,50 @@ function App() {
         <section id='core-concepts'>
                <h2>Core Concepts</h2>
                <ul>
-               <CoreConcept {...CORE_CONCEPTS[0]}
+                {CORE_CONCEPTS.map((conceptItem) => <CoreConcept key={conceptItem.title} {...conceptItem} /> )}
+              {/*  <CoreConcept {...CORE_CONCEPTS[0]}
                />
                <CoreConcept {...CORE_CONCEPTS[1]} />
                <CoreConcept {...CORE_CONCEPTS[2]} />
                <CoreConcept {...CORE_CONCEPTS[3]}
-               />
+               /> */}
                </ul>
         </section>
         <section id='examples'>
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Components</TabButton>
-            <TabButton>Components</TabButton>
+            <TabButton  isSelected={selectedTopic === 'components'}  onSelect={() => handleSelect('components')} >Components</TabButton>
+            <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect('jsx')} >JSX</TabButton>
+            <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect('props')} >Props</TabButton>
+            <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect('state')} >State</TabButton>
           </menu>
-        </section>
 
-      </main>
+          {tabContent}
+
+          {/* {!selectedTopic ? <p>Please Select a topic.</p> : (<div id='tab-content'>
+          <h3>{EXAMPLES[selectedTopic].title}</h3>
+          <p>{EXAMPLES[selectedTopic].description}</p>
+          <pre>
+            <code>
+                {EXAMPLES[selectedTopic].code}
+            </code>
+          </pre>
+         </div>) } */}
+        
+       
+       {/*     {!selectedTopic && <p>Please Select a topic.</p> }
+          {selectedTopic && (<div id='tab-content'>
+          <h3>{EXAMPLES[selectedTopic].title}</h3>
+          <p>{EXAMPLES[selectedTopic].description}</p>
+          <pre>
+            <code>
+                {EXAMPLES[selectedTopic].code}
+            </code>
+          </pre>
+         </div>)} */}
+         
+        </section>
+        </main>
     </div>
   );
 }
--- 01-starting-project/src/components/TabButton.jsx
+++ 01-starting-project/src/components/TabButton.jsx
@@ -1,3 +1,7 @@
-export default function TabButton({children}){
-    return <li><button>{children}</button></li>
+export default function TabButton({children, onSelect , isSelected}){
+    console.log('this is tabbutton executes 4 times')
+    return (
+    <li>
+        <button className={isSelected ? 'active' : undefined}  onClick={onSelect}>{children}</button>
+    </li>)
 }
